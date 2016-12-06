5ebfd4b95285877365e98de649198e2ae378eee8
markuskramerIgitt <markus.kramer@dpdhl.com>
2016-12-06 10:15:07 +0100

    simplify

--- ybuild.cmd
+++ ybuild.cmd
@@ -1,16 +1,12 @@
 @echo off
 
-
-:: Get the versions from salt/version.py
-for /f "delims=" %%a in ('python versionDisplayOrInternal.py i') do @set "iii=%%a"
-for /f "delims=" %%a in ('python versionDisplayOrInternal.py')   do @set "ddd=%%a"
-
-set iii=20.1.1.100
-set ddd=2020.1.1
-
-@echo %0 :: DisplayVersion  = %ddd%
-@echo %0 :: InternalVersion = %iii%
+:: just decoys
+@echo %0 :: DisplayVersion  = 2020.1.1
+@echo %0 :: InternalVersion = 20.1.1.100
 @echo.
 
 ::c:\windows\microsoft.net\framework\v4.0.30319
 "%ProgramFiles(x86)%"\MSBuild\12.0\Bin\msbuild.exe msbuild.proj /t:wix /p:TargetPlatform=amd64 /p:DisplayVersion=%ddd% /p:InternalVersion=%iii%
+
+@echo %0 :: dir wix\MinionMSI\bin\Release\
+dir wix\MinionMSI\bin\Release\
