7d6a9692897766c7d1464379ced7a7b6b70ecd9f
markuskramerIgitt <markus.kramer@posteo.de>
2017-01-21 20:41:18 +0100

    git describe options from version.py

--- wix/MinionMSI/versionDisplayOrInternal.py
+++ wix/MinionMSI/versionDisplayOrInternal.py
@@ -7,25 +7,28 @@ from __future__ import absolute_import, print_function
 
 import sys
 import subprocess
-version_git_describe = ""
-
+version_string = ""
+git_exe = r'"C:\Program Files\Git\cmd\git.exe" '
+git_opt = r'--git-dir=/git/salt/.git --work-tree=/git/salt '
+git_cmd = r'describe --tags --first-parent --match v[0-9]* --always'
+git_command_string = git_exe + git_opt + git_cmd
 try:
-	version_git_describe = str(subprocess.check_output (r'"C:\Program Files\Git\cmd\git.exe" --git-dir=/git/salt/.git --work-tree=/git/salt describe'))
-	#print (version_git_describe)
+	version_string = str(subprocess.check_output (git_command_string))
+	#print (version_string)
 except Exception as  e:
 	print (str(e))
 	sys.exit(1)
 	
-#print("version_git_describe = " + version_git_describe)
+#print("version_string = " + version_string)
 	
-version_list   = version_git_describe.replace('.',' ').replace('-',' ').split() # all elements are strings
+version_list   = version_string.replace('.',' ').replace('-',' ').split() # all elements are strings
 year    = version_list[0]
-minor   = version_list[1]
+month   = version_list[1]
 bugfix  = version_list[2]
-if year.startswith('v'):                 year                 = year[1:]
-if version_git_describe.startswith('v'): version_git_describe = version_git_describe[1:]
-DisplayVersion  =  version_git_describe    # Probably `git describe` becomes v2016.11.1 after a long series of 2016.11.1-2151-g559bee3 
-InternalVersion =  year[2:] + '.' + minor + '.' + bugfix 
+if year.startswith('v'):           year           = year[1:]
+if version_string.startswith('v'): version_string = version_string[1:]
+DisplayVersion  =  version_string    # Probably `git describe` becomes v2016.11.1 after a long series of 2016.11.1-2151-g559bee3 
+InternalVersion =  year[2:] + '.' + month + '.' + bugfix 
 if len(version_list) > 3: # mbugfix
 	InternalVersion += '.' + version_list[3]
 
