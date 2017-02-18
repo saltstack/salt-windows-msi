c4f654473ced90bfd3536686b6e890e2206475f5
markuskramerIgitt <markus.kramer@posteo.de>
2017-02-18 22:14:04 +0100

    researching the 32bit installer

--- ybuild.cmd
+++ ybuild.cmd
@@ -1,30 +1,42 @@
 @echo off
 
+if '%1'=='' (
+  set targetplatformvalue=amd64
+  goto :ok
+)
+
+if '%1'=='32' (
+  set targetplatformvalue=win32
+  goto :ok
+)
+
+echo wrong argument 
+echo    ybuild         == 64bit
+echo    ybuild 32      == 32bit
+goto :eof
+
+:ok
+
 :: Detecting Python...
 dir "C:\python27\python.exe" >nul 2>&1
 if not %errorLevel%==0 (
-    echo FATAL failure: This script needs Python in "C:\python27\python.exe"
-    goto eof
+  echo FATAL failure: This script needs Python in "C:\python27\python.exe"
+  goto eof
 )
 
 :: Detecting Salt repository...
 dir "C:\git\salt\.git" >nul 2>&1
 if not %errorLevel%==0 (
-    echo FATAL failure: This script needs the Salt repository  in "C:\git\salt\"
-    goto eof
+  echo FATAL failure: This script needs the Salt repository  in "C:\git\salt\"
+  goto eof
 )
 
 
-:: decoy values to understand the relationship between msbuild and WiX
-set ddd=2020.1.1
-set iii=20.1.1.100
-@echo %0 :: msbuild will get DisplayVersion  = %ddd%
-@echo %0 :: msbuild will get InternalVersion = %iii%
-@echo.
+:: decoy version values to understand the relationship between msbuild and WiX...
+@echo on
+call "%ProgramFiles(x86)%"\MSBuild\14.0\Bin\msbuild.exe msbuild.proj /nologo /t:wix /p:TargetPlatform=%targetplatformvalue% /p:DisplayVersion=2020.1.1 /p:InternalVersion=20.1.1.100
+@echo off
 
-call "%ProgramFiles(x86)%"\MSBuild\14.0\Bin\msbuild.exe msbuild.proj /nologo /t:wix /p:TargetPlatform=amd64 /p:DisplayVersion=%ddd% /p:InternalVersion=%iii%
-
-::@echo %0 :: result is in     wix.d\MinionMSI\bin\Release\
 dir                          wix.d\MinionMSI\bin\Release\*.msi
 
 
