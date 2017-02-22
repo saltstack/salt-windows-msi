567ab2a2c7f6f846188fddf8d0c9e656e5327db5
markuskramerIgitt <markus.kramer@posteo.de>
2017-02-23 00:36:44 +0100

    check before dir

--- zuninstall-KEEP_CONFIG-no.cmd
+++ zuninstall-KEEP_CONFIG-no.cmd
@@ -3,6 +3,10 @@
 for /f "delims=" %%a in ('dir /b wix.d\MinionMSI\bin\Release\*.msi')   do @set "msi=%%a"
 
 @echo on
-
 msiexec /x wix.d\MinionMSI\bin\Release\%msi% /qb! KEEP_CONFIG=0
-dir /s /b \salt
+@echo off
+
+dir "C:\salt" >nul 2>&1
+if %errorLevel%==0 (
+  dir /s /b c:\salt
+)
