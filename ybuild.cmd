fa23b8f899e8e026f99a858b47edc2ad3073f6b1
markuskramerIgitt <markuskramerIgitt@gmail.com>
2017-04-21 14:47:54 +0200

    Explicitly adding Platform

--- ybuild.cmd
+++ ybuild.cmd
@@ -1,13 +1,21 @@
 @echo off
 
+:: WISE STUDIO perceives the 64bit msi as 32bit msi.
+:: Explicitly adding Platform
+:: Platform should be x64 but this causes msbuild errror, therefor amd64
+::     C:\git\salt-windows-msi\wix.sln.metaproj : error MSB4126: Die angegebene Projektmappenkonfiguration "Release|x64" ist ungültig. [C:\git\salt-windows-msi\wix.sln]
+:: SuperOrca shows no difference between msi without Platformm and msi with Platform=amd64
+
 if '%1'=='' (
   set salt_targetplatform=amd64
+  set salt_platform=amd64
   set salt_bitness=64
   goto :ok
 )
 
 if '%1'=='32' (
   set salt_targetplatform=win32
+  set salt_platform=x86
   set salt_bitness=32
   goto :ok
 )
@@ -53,7 +61,7 @@ if not %errorLevel%==0 (
 
 :: decoy version values to understand the relationship between msbuild and WiX...
 @echo on
-call %msbuildpath%\msbuild.exe msbuild.proj /nologo /t:wix /p:TargetPlatform=%salt_targetplatform% /p:DisplayVersion=2020.1.1 /p:InternalVersion=20.1.1.100
+call %msbuildpath%\msbuild.exe msbuild.proj /nologo /t:wix /p:TargetPlatform=%salt_targetplatform% /p:Platform=%salt_platform% /p:DisplayVersion=2020.1.1 /p:InternalVersion=20.1.1.100
 @echo off
 
 dir                          wix.d\MinionMSI\bin\Release\*.msi
