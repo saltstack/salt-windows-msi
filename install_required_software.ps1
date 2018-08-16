42837343dd5d2aeaf656fcd395ef79bdf638da7f
markuskramerIgitt <markuskramerIgitt@gmail.com>
2018-08-16 12:35:54 +0200

    Indented

--- install_required_software.ps1
+++ install_required_software.ps1
@@ -1,36 +1,36 @@
 
 function Verify ($local_file, $SHA256) {
-  if (-Not (Test-Path $local_file)) {
-    Write-Host  -ForegroundColor Red "$local_file   MISSING"
-    exit -2
-  }
-  if ((Get-FileHash $local_file).Hash -eq $SHA256) {
-    Write-Host -ForegroundColor Green "Found $local_file"
-  } else {
-    Write-Host  -ForegroundColor Red "$local_file   UNEXPECTED HASH   $((Get-FileHash $local_file).Hash)"
-    exit -2
-  }
+    if (-Not (Test-Path $local_file)) {
+	Write-Host  -ForegroundColor Red "$local_file   MISSING"
+	exit -2
+    }
+    if ((Get-FileHash $local_file).Hash -eq $SHA256) {
+	Write-Host -ForegroundColor Green "Found $local_file"
+    } else {
+	Write-Host  -ForegroundColor Red "$local_file   UNEXPECTED HASH   $((Get-FileHash $local_file).Hash)"
+	exit -2
+    }
 }
 
 function VerifyOrDownload ($local_file, $URL, $SHA256) {
-  if (-Not (Test-Path $local_file)) {
-    "Downloading...  $URL"
-    [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-    (New-Object System.Net.WebClient).DownloadFile($URL, $local_file)
-  }
-  if ((Get-FileHash $local_file).Hash -eq $SHA256) {
-    Write-Host -ForegroundColor Green "Found $local_file"
-  } else {
-    Write-Host  -ForegroundColor Red "$local_file   UNEXPECTED HASH   $((Get-FileHash $local_file).Hash)"
-    exit -2
-  }
+    if (-Not (Test-Path $local_file)) {
+	"Downloading...  $URL"
+	[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+	(New-Object System.Net.WebClient).DownloadFile($URL, $local_file)
+    }
+    if ((Get-FileHash $local_file).Hash -eq $SHA256) {
+	Write-Host -ForegroundColor Green "Found $local_file"
+    } else {
+	Write-Host  -ForegroundColor Red "$local_file   UNEXPECTED HASH   $((Get-FileHash $local_file).Hash)"
+	exit -2
+    }
 }
 
 #### Path
-if(Test-Path -Path "c:/salt_msi_resources" ){
-  Write-Host -ForegroundColor Green  "Found c:/salt_msi_resources"
+if (Test-Path -Path "c:/salt_msi_resources") {
+    Write-Host -ForegroundColor Green  "Found c:/salt_msi_resources"
 } else {
-  New-Item -ItemType directory -Path "c:/salt_msi_resources"
+    New-Item -ItemType directory -Path "c:/salt_msi_resources"
 }
 
 #### Resources
@@ -47,28 +47,28 @@ Verify $f $h
 ## Wix 3.11
 ## http://wixtoolset.org/releases/
 if (Test-Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\"{AA06E868-267F-47FB-86BC-D3D62305D7F4}") {
-  Write-Host -ForegroundColor Green "Found Wix"
+    Write-Host -ForegroundColor Green "Found Wix"
 } else {
-  "For WiX 3.11 on Windows 10: is .Net 3.5 enabled?"
-  Start-Process optionalfeatures -Wait -NoNewWindow 
-
-  $wixInstaller = "c:/salt_msi_resources/wix311.exe"
-  $u = "https://github.com/wixtoolset/wix3/releases/download/wix3111rtm/wix311.exe"
-  $h = "7CAECC9FFDCDECA09E211AA20C8DD2153DA12A1647F8B077836B858C7B4CA265"
-  VerifyOrDownload $WixInstaller $u $h
-
-  Start-Process $wixInstaller -Wait -NoNewWindow
+    "For WiX 3.11 on Windows 10: is .Net 3.5 enabled?"
+    Start-Process optionalfeatures -Wait -NoNewWindow 
+    
+    $wixInstaller = "c:/salt_msi_resources/wix311.exe"
+    $u = "https://github.com/wixtoolset/wix3/releases/download/wix3111rtm/wix311.exe"
+    $h = "7CAECC9FFDCDECA09E211AA20C8DD2153DA12A1647F8B077836B858C7B4CA265"
+    VerifyOrDownload $WixInstaller $u $h
+    
+    Start-Process $wixInstaller -Wait -NoNewWindow
 }
 
 ## Build tools 2015
 ## https://www.microsoft.com/en-US/download/confirmation.aspx?id=48159
 if (Test-Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\"{8C918E5B-E238-401F-9F6E-4FB84B024CA2}") {
-  Write-Host -ForegroundColor Green "Found Build Tools"
+    Write-Host -ForegroundColor Green "Found Build Tools"
 } else {
-  $BuildToolsInstaller = "c:/salt_msi_resources/BuildTools_Full.exe"
-  $u = "https://download.microsoft.com/download/E/E/D/EEDF18A8-4AED-4CE0-BEBE-70A83094FC5A/BuildTools_Full.exe"
-  $h = "92CFB3DE1721066FF5A93F14A224CC26F839969706248B8B52371A8C40A9445B"
-  VerifyOrDownload $BuildToolsInstaller $u $h
-
-  Start-Process $BuildToolsInstaller -Wait -NoNewWindow
+    $BuildToolsInstaller = "c:/salt_msi_resources/BuildTools_Full.exe"
+    $u = "https://download.microsoft.com/download/E/E/D/EEDF18A8-4AED-4CE0-BEBE-70A83094FC5A/BuildTools_Full.exe"
+    $h = "92CFB3DE1721066FF5A93F14A224CC26F839969706248B8B52371A8C40A9445B"
+    VerifyOrDownload $BuildToolsInstaller $u $h
+    
+    Start-Process $BuildToolsInstaller -Wait -NoNewWindow
 }
