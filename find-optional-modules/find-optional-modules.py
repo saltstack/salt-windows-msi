83552e0bae2ca3634543b96dbd7162be3bdcdf37
markuskramerIgitt <markuskramerIgitt@gmail.com>
2019-02-21 21:33:28 +0100

    Count *.pyc, *.py and size of a module

--- find-optional-modules/find-optional-modules.py
+++ find-optional-modules/find-optional-modules.py
@@ -1,74 +1,82 @@
-# Identify optional components/features
+'''
+Find the  directories and files of optional modules.
+'''
 
 from __future__ import print_function
 import os
+import glob
+from humanreadable_bytes import humanreadable_bytes
 
-def get_count_size_from_dir(start_path = '.'):
-    total_size = 0
-    count = 0
-    for dirpath, dirnames, filenames in os.walk(start_path):
-        for f in filenames:
-            fp = os.path.join(dirpath, f)
-            total_size += os.path.getsize(fp)
-            count += 1
-    return count, total_size
+pyc_with_py = {}
+with open ('pyc_with_py.txt', 'r') as inputFile:
+    for pyc in inputFile.read().splitlines():
+        pyc_with_py[pyc] = 1
 
 
-def go_dir(dir, comment='', url=''):
-    count, size = get_count_size_from_dir(dir)
-    #print("{:>5} {:>10} {}".format(count, size, dir))
+def files_from_dir(dir):
+    ret = []
+    for dirpath, dirnames, filenames in os.walk(dir):
+        for filename in filenames:
+            full_file_name = os.path.join(dirpath, filename)
+            ret.append(full_file_name)
+    return ret
 
 
-def get_count_size_of_file_glob(dir):
-    total_size = 0
-    count = 0
-    import glob
-    for fp in glob.glob(dir):
-        total_size += os.path.getsize(fp)
-        count += 1
-    return count, total_size
-    
-
-def go_files(dir, comment='', url=''):
-    count, size = get_count_size_of_file_glob(dir)
-    print("{:>5} {:>10} {}".format(count, size, dir))
+def files_from_glob(file_pattern):
+    ret = []
+    for full_file_name in glob.glob(file_pattern):
+        ret.append(full_file_name)
+    assert len(ret) != 0, 'No files in {}'.format(file_pattern)
+    return ret
 
 
-go_dir(r'C:\salt\bin\Lib\site-packages\pip', 'Pip')
-go_dir(r'C:\salt\bin\Lib\site-packages\lxml', 'processing XML and HTML', 'https://lxml.de/')
-go_dir(r'C:\salt\bin\Lib\site-packages\salt\returners')
-go_dir(r'C:\salt\bin\Lib\ensurepip')
-go_dir(r'c:\salt\bin\Lib\distutils')
-go_dir(r'c:\salt\bin\Lib\email')
-go_dir(r'c:\salt\bin\Lib\hotshot', 'High-perfomance logging profiler')
-go_dir(r'c:\salt\bin\Lib\pydoc_data')
-go_dir(r'c:\salt\bin\Lib\xml')
-go_dir(r'C:\salt\bin\Lib\site-packages\git')
+def count_pyc_py_size(file):
+    total_size, pyccount, pycount = 0, 0, 0
+    if os.path.isdir(file): files = files_from_dir(file)
+    else:                   files = files_from_glob(file)
+    for full_file_name in files:
+        pyc1 = full_file_name.replace('C:/salt/', '').replace('c:/salt/', '').replace('\\', '/') + 'c'
+        if pyc1 in pyc_with_py:
+            pyccount += 1
+        total_size += os.path.getsize(full_file_name)
+        pycount += 1
+    return pyccount, pycount, total_size
 
-go_files(r'C:\salt\bin\Lib\doctest*.*', 'framework for running examples in docstrings')
-go_files(r'C:\salt\bin\Lib\pydoc*.*', 'Generate Python documentation in HTML or text')
-go_files(r'C:\salt\bin\Lib\mailbox.py', 'Read/write support for Maildir, mbox, MH, Babyl, and MMDF mailboxes')
 
-go_files(r'C:\salt\bin\Lib\site-packages\salt\states\git.*')
-go_files(r'C:\salt\bin\Lib\site-packages\salt\modules\git*.*')
+def desc_module(comment, files):
+    # Count *.pyc, *.py and size of a module
+    pyccount, pycount, size = 0, 0, 0
+    for file in files:
+        pyccount1, pycount1, size1 = count_pyc_py_size('C:/salt/'+file)
+        pyccount += pyccount1
+        pycount += pycount1
+        size += size1
+    print("{:>5} {:>10}  {:>10}   {}".format(pyccount, pycount, humanreadable_bytes(size), comment))
 
-go_files(r'C:\salt\bin\Lib\site-packages\salt\states\docker_*.*')
-go_files(r'C:\salt\bin\Lib\site-packages\salt\modules\docker*.*')
 
-go_files(r'C:\salt\bin\Lib\site-packages\salt\states\boto_*.*')
-go_files(r'C:\salt\bin\Lib\site-packages\salt\modules\boto*.*')
 
-go_files(r'C:\salt\bin\Lib\site-packages\salt\modules\vsphere*')
-
-go_files(r'c:\salt\bin\Lib\site-packages\salt\states\vagrant.py', 'Manange execution of Vagrant virtual machines on Salt minions')
-go_files(r'c:\salt\bin\Lib\site-packages\salt\states\virtualenv_mod.py', 'Setup of Python virtualenv sandboxes')
-go_files(r'c:\salt\bin\Lib\site-packages\salt\states\bigip.py', 'load-balancing configurations for F5 Big-IP')
-go_files(r'c:\salt\bin\Lib\site-packages\salt\states\esxi.py', 'VMware ESXi Hosts')
-go_files(r'c:\salt2\bin\Lib\site-packages\salt\states\grafana*.*', 'Grafana Dashboards')
-go_files(r'c:\salt2\bin\Lib\site-packages\salt\states\jboss7.py', 'Manage JBoss 7 Application Server via CLI interface')
-go_files(r'c:\salt2\bin\Lib\site-packages\salt\states\junos.py', 'Junos devices.  (Juniper Network Operating System)')
-go_files(r'')
-go_files(r'')
-go_files(r'')
+desc_module('Pip',       ['bin/Lib/site-packages/pip', 'bin/Lib/ensurepip'])
+desc_module('Distutils', ['bin/Lib/distutils'])
+desc_module('Setuptools', ['bin/Lib/site-packages/setuptools'])
+desc_module('Wheel', ['bin/Lib/site-packages/wheel'])
+desc_module('Processing XML and HTML', ['bin/Lib/site-packages/lxml'])
+desc_module('Returners', ['bin/Lib/site-packages/salt/returners'])
+desc_module('Email', ['bin/Lib/email'])
+desc_module('High-perfomance logging profiler', ['bin/Lib/hotshot'])
+desc_module('Pydoc_data', ['bin/Lib/pydoc_data'])
+desc_module('Framework for running examples in docstrings', ['bin/Lib/doctest*.*'], )
+desc_module('Generate Python documentation in HTML or text', ['bin/Lib/pydoc*.*'])
+desc_module('Read/write support for Maildir, mbox, MH, Babyl, and MMDF mailboxes', ['bin/Lib/mailbox.py'])
+desc_module('Git', ['bin/Lib/site-packages/git', 'bin/Lib/site-packages/salt/states/git.*', 'bin/Lib/site-packages/salt/modules/git*.*'])
+desc_module('Docker', ['bin/Lib/site-packages/salt/states/docker_*.*', 'bin/Lib/site-packages/salt/modules/docker*.*'])
+desc_module('Boto', ['bin/Lib/site-packages/salt/states/boto_*.*', 'bin/Lib/site-packages/salt/modules/boto*.*'])
+desc_module('Vsphere', ['bin/Lib/site-packages/salt/modules/vsphere*'])
+desc_module('Manange execution of Vagrant virtual machines on Salt minions', ['bin/Lib/site-packages/salt/states/vagrant.py'])
+desc_module('Setup of Python virtualenv sandboxes', ['bin/Lib/site-packages/salt/states/virtualenv_mod.py'])
+desc_module('Load-balancing configurations for F5 Big-IP', ['bin/Lib/site-packages/salt/states/bigip.py'])
+desc_module('VMware ESXi Hosts', ['bin/Lib/site-packages/salt/states/esxi.py'])
+desc_module('Grafana Dashboards', ['bin/Lib/site-packages/salt/states/grafana*.*'])
+desc_module('Manage JBoss 7 Application Server via CLI interface', ['bin/Lib/site-packages/salt/states/jboss7.py'])
+desc_module('Junos devices', ['bin/Lib/site-packages/salt/states/junos.py'])
 
 
