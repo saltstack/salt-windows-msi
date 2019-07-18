36418c29922d50a40e8118e6a7053f6f4d220cd9
markuskramerIgitt <markuskramerIgitt@gmail.com>
2019-07-18 18:40:29 +0200

    cleanup cmd files

--- uninstall.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-:: Get the file name
-for /f "delims=" %%a in ('dir /b wix.d\MinionMSI\bin\Release\*.msi')   do @set "msi=%%a"
-
-@echo on
-msiexec /x wix.d\MinionMSI\bin\Release\%msi% /qb!
-dir /s /b \salt
