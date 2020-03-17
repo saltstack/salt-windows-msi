b7a9db44ebe4008a0fa2a3b4611809fdf167e35c
markuskramerIgitt <markuskramerIgitt@gmail.com>
2020-03-17 14:09:44 +0100

    Added uninst.ps1

---
+++ uninst.ps1
@@ -0,0 +1,27 @@
+# Uninstalls the Salt-Minion using the msi upgrade code
+$upgradecodeRegKey = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UpgradeCodes\2A3BF6CFED569A14DA191DA004B26D14'
+
+
+if (Test-Path $upgradecodeRegKey) {           # The scrambled product code is the only entry
+    $scrambledProductcode = $((Get-ItemProperty $upgradecodeRegKey).PSObject.Properties)[0].Name.ToString()
+    if ($scrambledProductcode -match '^[0-9A-Z]{32}$') {            # Reverse chunks
+        #Write-Host -ForegroundColor Yellow $scrambledProductcode.Insert(20,'-').Insert(16,'-').Insert(12,'-').Insert(8,'-')
+        $productcode = ""
+        $lastIndex = 0
+        foreach ($index in @(8, 4, 4, 2, 2, 2, 2, 2, 2, 2, 2)) {
+            $chunk = $scrambledProductcode.Substring($lastIndex, $index) -split ''
+            [array]::Reverse($chunk)
+            $productcode += $chunk -join ''
+            $lastIndex += $index
+        }
+        # Format productcode with dashes and curly braces
+        $productcode = $productcode.Insert(20,'-').Insert(16,'-').Insert(12,'-').Insert(8,'-')
+        $productcode = "{$productcode}"
+        & msiexec /x $productcode /qb
+    } else {
+        Write-Host -ForegroundColor Red Cannot uninstall
+        exit(1)
+    }
+} else {
+    Write-Host -ForegroundColor Blue Not installed
+}
