d7f1476e4b9835aec450b375150510e7e052f3d3
Markus Kramer <markuskramerIgitt@gmail.com>
2020-05-25 15:02:23 +0200

    Utilities for testing 32/64bit setups

---
+++ test_mock_nsis64.bat
@@ -0,0 +1,4 @@
+mkdir    C:\git\salt\pkg\windows\buildenv\bin
+mkdir    C:\git\salt\pkg\windows\installer
+echo 123>C:\git\salt\pkg\windows\buildenv\bin\ssm.exe
+echo 123>C:\git\salt\pkg\windows\installer\Salt-Minion-3210-Py3-AMD64-Setup.exe
