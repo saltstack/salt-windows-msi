919ceb265f81a656d29e2c1e0cb438138dedad8c
Markus Kramer <markuskramerIgitt@gmail.com>
2021-09-26 13:08:12 +0200

    debugging

--- CustomAction01/debug_dotnetframework_csharp/Program.cs
+++ CustomAction01/debug_dotnetframework_csharp/Program.cs
@@ -25,13 +25,16 @@ namespace MinionConfigurationExtension {
 
     class Program {
 
-        private static void write_master_and_id_to_file_IMCAC(Session session, String configfile, List<string> multimasters, String id) {
+        private static void write_master_and_id_to_file_DECAC(Session session, String configfile, string csv_multimasters, String id) {
             /* How to
              * read line
              * if line master, read multimaster, replace
              * if line id, replace
              * copy through line
             */
+            char[] separators = new char[] { ',', ' ' };
+            string[] multimasters = csv_multimasters.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
             session.Log("...want to write master and id to " + configfile);
             bool configExists = File.Exists(configfile);
             session.Log("......file exists " + configExists);
@@ -41,56 +44,65 @@ namespace MinionConfigurationExtension {
                 configLinesINPUT = File.ReadAllLines(configfile);
             }
             session.Log("...found config lines count " + configLinesINPUT.Length);
-            session.Log("...got master count " + multimasters.Count);
+            session.Log("...got master count " + multimasters.Length);
             session.Log("...got id " + id);
 
-            Regex line_contains_key = new Regex(@"^([a-zA-Z_]+):\s*");
+            Regex line_contains_key = new Regex(@"^([a-zA-Z_]+):");
             Regex line_contains_one_multimaster = new Regex(@"^\s*-\s*([0-9a-zA-Z_.-]+)\s*$");
             bool master_emitted = false;
             bool id_emitted = false;
 
             bool look_for_multimasters = false;
             foreach (string line in configLinesINPUT) {
-                if (look_for_multimasters) {
-                    // consume multimasters
-                    {
-                        if (line_contains_one_multimaster.IsMatch(line)) {
-                            // consume another multimaster
-                        } else {
-                            if(multimasters.Count == 1) {
-                                configLinesOUTPUT.Add("master: " + multimasters[0]);
-                                master_emitted = true;
-                            }
-                            if (multimasters.Count > 1) {
-                                configLinesOUTPUT.Add("master:");
-                                foreach(string onemultimaster in multimasters) {
-                                    configLinesOUTPUT.Add("- " + onemultimaster);
-                                }
-                                master_emitted = true;
-                            }
+                // search master and id
+                if (line_contains_key.IsMatch(line)) {
+                    Match m = line_contains_key.Match(line);
+                    string key = m.Groups[1].ToString();
+                    if (key == "master") {
+                        look_for_multimasters = true;
+                        continue; // next line
+                    } else if (key == "id") {
+                        // emit id
+                        configLinesOUTPUT.Add("id: " + id);
+                        id_emitted = true;
+                        continue; // next line
+                    } else {
+                        if (!look_for_multimasters) {
                             configLinesOUTPUT.Add(line); // copy through
-                            look_for_multimasters = false;
+                            continue; // next line
                         }
                     }
                 } else {
-                    // search master and id
-                    if (line_contains_key.IsMatch(line)) {
-                        Match m = line_contains_key.Match(line);
-                        string key = m.Groups[1].ToString();
-                        if (key == "master") {
-                            look_for_multimasters = true;
-                        } else if (key == "id") {
-                            configLinesOUTPUT.Add("id: " + id);
-                            id_emitted = true;
-                        } else {
-                            configLinesOUTPUT.Add(line); // copy through
-                        }
-                    } else {
+                    if (!look_for_multimasters) {
                         configLinesOUTPUT.Add(line); // copy through
+                        continue; // next line
+                    }
+                }
+
+                if (look_for_multimasters) {
+                    // consume multimasters
+                    if (line_contains_one_multimaster.IsMatch(line)) {
+                        // consume another multimaster
+                    } else {
+                        look_for_multimasters = false;
+                        // First emit master
+                        if (multimasters.Length == 1) {
+                            configLinesOUTPUT.Add("master: " + multimasters[0]);
+                            master_emitted = true;
+                        }
+                        if (multimasters.Length > 1) {
+                            configLinesOUTPUT.Add("master:");
+                            foreach (string onemultimaster in multimasters) {
+                                configLinesOUTPUT.Add("- " + onemultimaster);
+                            }
+                            master_emitted = true;
+                        }
+                        configLinesOUTPUT.Add(line); // Then copy through whatever is not one multimaster
                     }
                 }
             }
 
+            // input is read
             if (!master_emitted) {
                 // put master after hash master
                 Regex line_contains_hash_master = new Regex(@"^# master:");
@@ -98,11 +110,11 @@ namespace MinionConfigurationExtension {
                 foreach (string output_line in configLinesOUTPUT) {
                     configLinesOUTPUT_hash_master.Add(output_line);
                     if(line_contains_hash_master.IsMatch(output_line)) {
-                        if (multimasters.Count == 1) {
+                        if (multimasters.Length == 1) {
                             configLinesOUTPUT_hash_master.Add("master: " + multimasters[0]);
                             master_emitted = true;
                         }
-                        if (multimasters.Count > 1) {
+                        if (multimasters.Length > 1) {
                             configLinesOUTPUT_hash_master.Add("master:");
                             foreach (string onemultimaster in multimasters) {
                                 configLinesOUTPUT_hash_master.Add("- " + onemultimaster);
@@ -115,10 +127,10 @@ namespace MinionConfigurationExtension {
             }
             if (!master_emitted) {
                 // put master at end
-                if (multimasters.Count == 1) {
+                if (multimasters.Length == 1) {
                     configLinesOUTPUT.Add("master: " + multimasters[0]);
                 }
-                if (multimasters.Count > 1) {
+                if (multimasters.Length > 1) {
                     configLinesOUTPUT.Add("master:");
                     foreach (string onemultimaster in multimasters) {
                         configLinesOUTPUT.Add("- " + onemultimaster);
@@ -302,11 +314,11 @@ namespace MinionConfigurationExtension {
 
             String the_master= "";
             String the_id = "bob";
-            List<string> the_multimasters = new List<string>() {"anna1" ,"anna2" };
+            string the_multimasters = "anna1,anna2";
 
             //read_master_and_id_from_file_IMCAC(the_session, @"c:\temp\testme.txt", ref the_master, ref the_id);
 
-            write_master_and_id_to_file_IMCAC(the_session, @"c:\temp\testme.txt", the_multimasters, the_id);
+            write_master_and_id_to_file_DECAC(the_session, @"c:\temp\testme.txt", the_multimasters, the_id);
 
         }
     }
