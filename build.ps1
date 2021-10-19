03e0fd7692f6534004c678ae6793924ed684c883
Elias Probst <e.probst@ssc-services.de>
2021-10-19 14:07:33 +0000

    Correct project domain

--- build.ps1
+++ build.ps1
@@ -102,19 +102,19 @@ if ((ProductcodeExists "{8C918E5B-E238-401F-9F6E-4FB84B024CA2}") -or
 
 ## VC++ Runtime 2013
 VerifyOrDownload "$pwd\_cache.dir\Microsoft_VC120_CRT_x64.msm" `
-    "http://repo.saltstack.com/windows/dependencies/64/Microsoft_VC120_CRT_x64.msm" `
+    "http://repo.saltproject.io/windows/dependencies/64/Microsoft_VC120_CRT_x64.msm" `
     "15FD10A495287505184B8913DF8D6A9CA461F44F78BC74115A0C14A5EDD1C9A7"
 VerifyOrDownload "$pwd\_cache.dir\Microsoft_VC120_CRT_x86.msm" `
-    "http://repo.saltstack.com/windows/dependencies/32/Microsoft_VC120_CRT_x86.msm" `
+    "http://repo.saltproject.io/windows/dependencies/32/Microsoft_VC120_CRT_x86.msm" `
     "26340B393F52888B908AC3E67B935A80D390E1728A31FF38EBCEC01117EB2579"
 
 ## VC++ Runtime 2015
 VerifyOrDownload "$pwd\_cache.dir\Microsoft_VC140_CRT_x64.msm" `
-    "http://repo.saltstack.com/windows/dependencies/64/Microsoft_VC140_CRT_x64.msm" `
+    "http://repo.saltproject.io/windows/dependencies/64/Microsoft_VC140_CRT_x64.msm" `
     "E1344D5943FB2BBB7A56470ED0B7E2B9B212CD9210D3CC6FA82BC3DA8F11EDA8"
 
 VerifyOrDownload "$pwd\_cache.dir\Microsoft_VC140_CRT_x86.msm" `
-    "http://repo.saltstack.com/windows/dependencies/32/Microsoft_VC140_CRT_x86.msm" `
+    "http://repo.saltproject.io/windows/dependencies/32/Microsoft_VC140_CRT_x86.msm" `
     "0D36CFE6E9ABD7F530DBAA4A83841CDBEF9B2ADCB625614AF18208FDCD6B92A4"
 
 
